ca8766cc52160b1be2a2de5f0a853cbc8afcc98c
duanluan <duanluan@outlook.com>
2025-05-12 04:24:01 +0800

    feat: Timezone 下拉框组件 + 时间戳转日期时间

--- composeApp/src/desktopMain/kotlin/top/zhjh/zutil/composable/TimestampTool.kt
+++ composeApp/src/desktopMain/kotlin/top/zhjh/zutil/composable/TimestampTool.kt
@@ -1,66 +1,143 @@
 package top.zhjh.zutil.composable
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import compose.icons.FeatherIcons
+import compose.icons.feathericons.ArrowRightCircle
+import compose.icons.feathericons.Clipboard
+import compose.icons.feathericons.RefreshCw
+import compose.icons.feathericons.Shuffle
 import kotlinx.coroutines.delay
 import top.csaf.awt.ClipboardUtil
 import top.csaf.date.DateUtil
+import top.zhjh.zutil.common.composable.MyTextField
+import java.util.*
 
 @Composable
 fun TimestampTool() {
   // 添加通知宿主
   ToastContainer()
 
-  // 是否为毫秒级时间戳
-  var isMilliseconds by remember { mutableStateOf(false) }
-  var currentTimestamp by remember { mutableStateOf(DateUtil.nowEpochSecond()) }
+  Column {
+    // 当前时间戳是否为毫秒级
+    var isMilliByCurrent by remember { mutableStateOf(false) }
+    // 当前时间戳
+    var currentTimestamp by remember { mutableStateOf(DateUtil.nowEpochSecond()) }
 
-  // 启动协程定期更新时间戳
-  LaunchedEffect(key1 = true, key2 = isMilliseconds) {
-    while (true) {
-      currentTimestamp = if (isMilliseconds) {
-        DateUtil.nowEpochMilli()
-      } else {
-        DateUtil.nowEpochSecond()
-      }
-      // 秒级时间戳每秒更新一次，毫秒级时间戳每100毫秒更新一次
-      delay(if (isMilliseconds) 100 else 1000)
-    }
-  }
-
-  Column(modifier = Modifier.padding(16.dp)) {
-    Text(
-      text = "当前时间戳：${currentTimestamp}",
-      style = MaterialTheme.typography.h5
-    )
-
-    Row(modifier = Modifier.padding(top = 8.dp), horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-      // 复制时间戳
-      Button(
-        onClick = {
-          val msg = (if (ClipboardUtil.set(currentTimestamp)) {
-            ToastManager.success("复制成功")
-          } else {
-            ToastManager.error("复制失败")
-          })
+    // 启动协程定期更新时间戳
+    LaunchedEffect(key1 = true, key2 = isMilliByCurrent) {
+      while (true) {
+        currentTimestamp = if (isMilliByCurrent) {
+          DateUtil.nowEpochMilli()
+        } else {
+          DateUtil.nowEpochSecond()
         }
-      ) {
-        Text(text = "复制时间戳")
+        // 秒级每秒更新一次，毫秒级每100毫秒更新一次
+        delay(if (isMilliByCurrent) 100 else 1000)
       }
+    }
 
-      // 切换秒和毫秒
-      Button(
-        onClick = { isMilliseconds = !isMilliseconds }
-      ) {
-        Text(text = if (isMilliseconds) "切换为秒级" else "切换为毫秒级")
+    Box(modifier = Modifier.padding(10.dp).fillMaxWidth().border(1.dp, Color(0xffe5e7eb), RoundedCornerShape(10.dp))) {
+      Column(modifier = Modifier.padding(10.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Text(text = "当前时间戳：${currentTimestamp}", style = MaterialTheme.typography.h6)
+
+        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+          // 复制时间戳
+          Button(onClick = {
+            (if (ClipboardUtil.set(currentTimestamp)) {
+              ToastManager.success("复制成功")
+            } else {
+              ToastManager.error("复制失败")
+            })
+          }
+          ) {
+            val text = "复制时间戳"
+            Icon(modifier = Modifier.size(18.dp), imageVector = FeatherIcons.Clipboard, contentDescription = text)
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(text)
+          }
+
+          // 切换秒和毫秒
+          Button(modifier = Modifier.width(148.dp), onClick = { isMilliByCurrent = !isMilliByCurrent }) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+              val text = if (isMilliByCurrent) "切换为秒级" else "切换为毫秒级"
+              Icon(modifier = Modifier.size(18.dp), imageVector = FeatherIcons.Shuffle, contentDescription = text)
+              Spacer(modifier = Modifier.width(5.dp))
+              Text(text)
+            }
+          }
+        }
+      }
+    }
+
+    Box(modifier = Modifier.padding(10.dp).fillMaxWidth().border(1.dp, Color(0xffe5e7eb), RoundedCornerShape(10.dp))) {
+      Column(modifier = Modifier.padding(10.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        // 需要转换的时间戳
+        val toDatetimeTimestamp = remember { mutableStateOf(currentTimestamp.toString()) }
+        // 转换后的日期时间
+        val toDatetime = remember { mutableStateOf("") }
+        // 选择的时区
+        var selectedTimezone by remember { mutableStateOf(TimeZone.getDefault().id) }
+
+        Text(text = "时间戳转日期时间", style = MaterialTheme.typography.h6)
+        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+          MyTextField(modifier = Modifier.width(180.dp), value = toDatetimeTimestamp.value, onValueChange = { toDatetimeTimestamp.value = it })
+          Icon(imageVector = FeatherIcons.ArrowRightCircle, contentDescription = "转换")
+          MyTextField(modifier = Modifier.width(180.dp), value = toDatetime.value, onValueChange = { toDatetime.value = it })
+          TimezoneDropdown(
+            selectedTimezone = selectedTimezone,
+            onTimezoneSelected = { newTimezone ->
+              selectedTimezone = newTimezone
+            }
+          )
+        }
+
+        Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+          // 当前时间戳是否为毫秒级
+          var isMilliToDatetime by remember { mutableStateOf(false) }
+          // 切换秒和毫秒
+          Button(modifier = Modifier.width(148.dp), onClick = {
+            isMilliToDatetime = !isMilliToDatetime
+            if (isMilliToDatetime) {
+              toDatetimeTimestamp.value = toDatetimeTimestamp.value + "000"
+            } else {
+              toDatetimeTimestamp.value = toDatetimeTimestamp.value.substring(0, 10)
+            }
+          }) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+              val text = if (isMilliToDatetime) "切换为秒级" else "切换为毫秒级"
+              Icon(modifier = Modifier.size(18.dp), imageVector = FeatherIcons.Shuffle, contentDescription = text)
+              Spacer(modifier = Modifier.width(5.dp))
+              Text(text)
+            }
+          }
+
+          Button(onClick = {
+            // 转换时间戳
+            var timestamp = toDatetimeTimestamp.value.toLongOrNull()
+            if (timestamp != null) {
+              if (!isMilliToDatetime) {
+                timestamp *= 1000
+              }
+              toDatetime.value = DateUtil.format(timestamp, TimeZone.getTimeZone(selectedTimezone).toZoneId())
+            } else {
+              ToastManager.error("时间戳格式错误")
+            }
+          }) {
+            Icon(modifier = Modifier.size(18.dp), imageVector = FeatherIcons.RefreshCw, contentDescription = "转换")
+            Spacer(modifier = Modifier.width(5.dp))
+            Text("转换")
+          }
+        }
       }
     }
   }
---
+++ composeApp/src/desktopMain/kotlin/top/zhjh/zutil/composable/TimezoneDropdown.kt
@@ -0,0 +1,73 @@
+package top.zhjh.zutil.composable
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.toSize
+import top.zhjh.zutil.common.composable.MyTextField
+import java.util.*
+
+@Composable
+fun TimezoneDropdown(
+  // 当前选中的时区
+  selectedTimezone: String,
+  // 时区变化时的回调函数
+  onTimezoneSelected: (String) -> Unit
+) {
+  // 获取所有可用的时区ID
+  val timezones = remember { TimeZone.getAvailableIDs().sortedArray() }
+
+  // 控制下拉菜单的展开状态
+  var expanded by remember { mutableStateOf(false) }
+
+  // 用于设置下拉菜单宽度
+  var textFieldSize by remember { mutableStateOf(Size.Zero) }
+  val localDensity = LocalDensity.current
+
+  Column {
+    // 输入框作为下拉菜单的触发器
+    MyTextField(
+      value = selectedTimezone,
+      // 使用外部传入的回调
+      onValueChange = { onTimezoneSelected(it) },
+      modifier = Modifier.clickable { expanded = true }
+        .onGloballyPositioned { coordinates ->
+          // 获取文本框尺寸
+          textFieldSize = coordinates.size.toSize()
+        },
+      // 解决 clickable 无效：https://stackoverflow.com/a/69198950/11403802
+      enabled = false,
+      readOnly = true,
+      trailingIcon = { Icon(Icons.Filled.ArrowDropDown, contentDescription = "下拉箭头") }
+    )
+
+    // 下拉菜单
+    DropdownMenu(
+      expanded = expanded,
+      onDismissRequest = { expanded = false },
+      modifier = Modifier.width(with(localDensity) { textFieldSize.width.toDp() })
+    ) {
+      timezones.forEach { timezone ->
+        DropdownMenuItem(
+          content = { Text(text = timezone) },
+          onClick = {
+            // 使用外部传入的回调
+            onTimezoneSelected(timezone)
+            expanded = false
+          }
+        )
+      }
+    }
+  }
+}
